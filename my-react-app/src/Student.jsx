9d7cc06286a5e1d36484fbc838a255f54f1cbadb
peterGwapo29 <peterolanolan@gmail.com>
2025-06-12 15:46:31 +0800

    .map() .filter() .sort()

--- my-react-app/src/Student.jsx
+++ my-react-app/src/Student.jsx
@@ -1,43 +1,66 @@
 
 function Student() {
 
-    const studentList = [
+    const book_List = [
         {
             id: 1,
-            name: "Peter",
-            age: 19,
-            course: "BSIT"
+            title: "Math",
+            author: "Peter",
+            genre: "Fiction",
+            available: true
         },
         {
             id: 2,
-            name: "Gwapo",
-            age: 20,
-            course: "BSBA"
+            title: "Science",
+            author: "Peter",
+            genre: "Science",
+            available: false
         },
         {
             id: 3,
-            name: "Handsome",
-            age: 21,
-            course: "BSA"
+            title: "AP",
+            author: "Peter",
+            genre: "Fiction",
+            available: true
+        },
+        {
+            id: 4,
+            title: "IM",
+            author: "Peter",
+            genre: "History",
+            available: true
+        },
+        {
+            id: 5,
+            title: "ITC",
+            author: "Peter",
+            genre: "Science",
+            available: false
         },
 
     ];
 
-    const result = studentList.map(student =>
-                                    <li key={student.id}>
-                                        Name: {student.name}
-                                        Age: {student.age}
-                                        Course: {student.course}
-                                    </li> );
-                                    
+    const availableBooks = book_List.filter(avBook =>   avBook.available === true &&
+                                                        avBook.genre === "Fiction");
+
+
+    availableBooks.sort( (a, b) => a.title.localeCompare(b.title));                                                    
+    const books =  availableBooks.map(book => 
+        <li key={book.id}>
+            Title: {book.title}, 
+            Author: {book.author}, 
+            Genre: {book.genre}
+        </li>
+    );
+
 
     return(
         <>
             <main className="main-container">
                 <div className="content">
-                    <h1>Student</h1>
+                    <h1>Books</h1>
                     <div className="log">
-                    <ol>{result}</ol>
+                    <ol>{books}</ol>
                     </div>
                 </div>
             </main>
