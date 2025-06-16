ffc242db032fdadbfac9a61a9c080c394d0fd313
peterGwapo29 <peterolanolan@gmail.com>
2025-06-16 21:07:21 +0800

    to do list

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,27 +1,29 @@
-import Header from './Header.jsx'
-// import Student from './Student.jsx'
-// import Button from './Button.jsx'
-import Component from './Component.jsx'
-import ColorPicker from './ColorPicker.jsx'
-import MyComponent from './MyComponent.jsx'
-import Food from './Food.jsx'
-import Car from './Car.jsx'
-import Foods from './Foods.jsx'
+// import Header from './Header.jsx'
+// // import Student from './Student.jsx'
+// // import Button from './Button.jsx'
+// import Component from './Component.jsx'
+// import ColorPicker from './ColorPicker.jsx'
+// import MyComponent from './MyComponent.jsx'
+// import Food from './Food.jsx'
+// import Car from './Car.jsx'
+// import Foods from './Foods.jsx'
+import ToDoList from './ToDoList.jsx'
 
 
 function App() {
   return(
     <>
-      <Header/>
+      {/* <Header/> */}
       {/* <Student/> */}
       {/* <Button/> */}
-    <Component/>
+    {/* <Component/>
     <br />
     <ColorPicker/>
     <MyComponent/>
     <Food/>
     <Car/>
-    <Foods/>
+    <Foods/> */}
+    <ToDoList/>
     
       
     </>
---
+++ my-react-app/src/ToDoList.jsx
@@ -0,0 +1,75 @@
+import React, {useState} from 'react';
+
+function ToDoList() {
+    const [task, setTask] = useState(["Go to work", "Go to gym"]);
+    const [newTask, setNewTask] = useState("");
+
+    function HandleChangeInput(event){
+        setNewTask(event.target.value);
+    }
+
+    function AddTask() {
+        if (newTask.trim() === "") {
+            alert("Please enter a task");
+            return;
+        }
+        setTask([...task, newTask]);
+        setNewTask("");
+    }
+
+    function DeleteTask(index) {
+        const updateTask = task.filter((_, i) => i !== index);
+        setTask(updateTask);
+    }
+
+    function MoveUp(index) {
+        if (index === 0) return;
+        const updatedTasks = [...task];
+        [updatedTasks[index - 1], updatedTasks[index]] = [updatedTasks[index], updatedTasks[index - 1]];
+        setTask(updatedTasks);
+    }
+
+    function MoveDown(index) {
+        if (index === task.length - 1) return;
+        const updatedTasks = [...task];
+        [updatedTasks[index + 1], updatedTasks[index]] = [updatedTasks[index], updatedTasks[index + 1]];
+        setTask(updatedTasks);
+    }
+    
+    return(
+        <div className="myTodos">
+            <h1>To-Do-List</h1>
+
+            <input  type="text" 
+                    placeholder='Enter To do'
+                    value={newTask}
+                    onChange={HandleChangeInput}
+            />
+
+            <button id='add-task-button' onClick={AddTask}>Add task</button>
+
+            <ul>
+                {task.map((todo, ind) => 
+                            <li key={ind}>
+                                <span>{todo}</span>
+
+                                <button id='delete-button' onClick={() => DeleteTask(ind) }>Delete</button>
+                                
+                                <button className='move'
+                                        id='move-up-button'
+                                        onClick={() => MoveUp(ind)}>
+                                    Up
+                                </button>
+
+                                <button className='move'
+                                        id='move-down-button'
+                                        onClick={() => MoveDown(ind)}>
+                                    Down
+                                </button>
+                            </li>)}
+            </ul>
+        </div>
+    );
+}
+
+export default ToDoList
